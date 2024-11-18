1c3fb3a8f71fa13e02dd2dc8c3b580209aac3eaf
Rafik <s94rfarh@uni-bonn.de>
2024-11-18 16:09:52 +0100

    building models and applying them so they are available in the database

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    return render(request, 'posts/posts_list.html') #Ort des templates, es weiß wo der allg. Template Ordner ist, wegen settings.py
