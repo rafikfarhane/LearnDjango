7a8005358a8ebd5c0c6f4d955a7be86016ba28ed
Rafik <s94rfarh@uni-bonn.de>
2024-11-18 18:47:05 +0100

    using the admin-panel to mange content and ORM to work quicker in html and avoiding repetitive code

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html') #Ort des templates, es weiß wo der allg. Template Ordner ist, wegen settings.py
+    posts = Post.objects.all().order_by('-date') #gets all the post-objeacts
+    return render(request, 'posts/posts_list.html', {'posts': posts}) #Ort des templates, es weiß wo der allg. Template Ordner ist, wegen settings.py
+#1.bring post-model
+#2.get all the posts
+#3 pass the data on the template, so we can use it in our post-list template
+# third param. in render()
+#für 'posts' wird das query-set posts gespeichert
